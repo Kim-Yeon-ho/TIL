c6beb0ff0688a1cbee7ae8561645a86424987e5a
LeeJungHwan-Dev <aftermoa@gmail.com>
2022-08-24 14:30:05 +0900

    Add : LinearLayout Example 추가

---
+++ Lee_Android_Study/Ch02_Layout/app/src/main/java/com/example/linearlayout/LinearActivity.kt
@@ -0,0 +1,11 @@
+package com.example.linearlayout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LinearActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_layout)
+    }
+}
---
+++ Lee_Android_Study/Ch02_Layout/app/src/main/java/com/example/linearlayout/RelativeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.linearlayout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RelativeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.relative_layout)
+    }
+}
